905dc276862b06179056f7e40760ce8da51bdbb4
WoIfey <wuflerz@outlook.com>
2024-02-13 13:54:38 +0200

    Update

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,13 +1,12 @@
 "use server"
 import { deleteData, saveData, updateData } from "@/utils/handleDatabase"
-import { revalidateTag } from "next/cache"
+import { revalidatePath } from "next/cache"
 
 export const create = async (formData: FormData) => {
-    'use server'
     const quote = formData.get('quote') as string
     const author = formData.get('author') as string
     await saveData(quote, author)
-    revalidateTag('quote')
+    revalidatePath('/')
     console.log('Created')
 }
 
@@ -17,12 +16,12 @@ export const update = async (formData: FormData) => {
     const author = formData.get('author') as string
     const data = await updateData(id, author, quote)
     console.log(data)
-    revalidateTag('quote')
+    revalidatePath('/')
 }
 
 export const remove = async (formData: FormData) => {
     const id = formData.get('id') as string
     const data = await deleteData(id)
     console.log(data)
-    revalidateTag('quote')
+    revalidatePath('/')
 }
