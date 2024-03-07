59dc45c6b6a44dad973fda7e01116816c602829d
WoIfey <wuflerz@outlook.com>
2024-03-07 12:47:00 +0200

    Default Sort

--- src/components/sort.tsx
+++ src/components/sort.tsx
@@ -12,9 +12,10 @@ export default function Sort() {
 	const handleSort = () => {
 		if (sortPreference === 'Sort by newest') {
 			localStorage.setItem('sortPreference', 'new')
-		}
-		if (sortPreference === 'Sort by oldest') {
+		} else if (sortPreference === 'Sort by oldest') {
 			localStorage.setItem('sortPreference', 'old')
+		} else if (sortPreference === 'Sort by default') {
+			localStorage.removeItem('sortPreference')
 		}
 	}
 
@@ -30,6 +31,7 @@ export default function Sort() {
 				onChange={handleSortChange}
 				className="bg-slate-700 mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 ring-1 ring-inset focus:ring-2 ring-slate-500 focus:ring-indigo-600 sm:text-sm sm:leading-6"
 			>
+				<option>Sort by default</option>
 				<option>Sort by newest</option>
 				<option>Sort by oldest</option>
 			</select>
